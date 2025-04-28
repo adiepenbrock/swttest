8c9be4e988095f8ca56aa8c485a2ad3e85953dad
Andreas Diepenbrock <mail@adiepenbrock.de>
2025-04-28 15:18:56 +0200

    feat: create spring project
    ref: #1

---
+++ appone/src/main/java/de/fhdo/swt/teamone/appone/ApponeApplication.java
@@ -0,0 +1,13 @@
+package de.fhdo.swt.teamone.appone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApponeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApponeApplication.class, args);
+    }
+
+}
---
+++ appone/src/test/java/de/fhdo/swt/teamone/appone/ApponeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fhdo.swt.teamone.appone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApponeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
